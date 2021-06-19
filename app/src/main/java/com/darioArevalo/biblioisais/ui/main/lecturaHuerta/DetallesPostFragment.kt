42ce6a1331a9bef55d8a523ecb20a14fe2490440
crixtiam <cristiam9211@gmail.com>
2021-06-19 02:15:52 -0500

    Add new Post ok v2, img ok, design ok

--- app/src/main/java/com/darioArevalo/biblioisais/ui/main/lecturaHuerta/DetallesPostFragment.kt
+++ app/src/main/java/com/darioArevalo/biblioisais/ui/main/lecturaHuerta/DetallesPostFragment.kt
@@ -1,12 +1,14 @@
 package com.darioArevalo.biblioisais.ui.main.lecturaHuerta
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
@@ -68,17 +70,15 @@ class DetallesPostFragment : Fragment() {
             when(result){
                 is Result.Loading->{
                     Log.d("Livedata","Loading...")
-                    //binding.progressBarLecturaHuerta.visibility = View.VISIBLE
+                    binding.progressBarDetallesPost.visibility = View.VISIBLE
                 }
                 is Result.Success->{
-                    //binding.progressBarLecturaHuerta.visibility = View.GONE
+                    binding.progressBarDetallesPost.visibility = View.GONE
                     Log.d("Livedata_comment","${result.data}")
                     binding.rvPostDetalles.adapter = commentAdapter(result.data)
-                    //binding.rvPostList.adapter = LecturaHuertaAdapter(result.data)
                 }
 
                 is Result.Failure->{
-                    //binding.progressBarLecturaHuerta.visibility = View.GONE
                     Log.d("livedata error","{${result.exception}}")
                 }
             }
@@ -86,9 +86,21 @@ class DetallesPostFragment : Fragment() {
         })
 
         binding.btnComment.setOnClickListener {
-            val commentPost = binding.editTxtContent.text
+            val commentPost = binding.editTxtContent.text.toString()
             val keyPost = post.post_Id
-            viewModel.addNewComment(commentPost.toString(),keyPost)
+
+            if (TextUtils.isEmpty(commentPost)){
+
+                Toast.makeText(context,"Tienes Espacios Vacios",Toast.LENGTH_SHORT).show()
+
+            }else{
+                viewModel.addNewComment(commentPost,keyPost)
+                binding.editTxtContent.setText("")
+                Toast.makeText(this.context,"Has comentado",Toast.LENGTH_SHORT).show()
+
+            }
+
+
         }
 
 
