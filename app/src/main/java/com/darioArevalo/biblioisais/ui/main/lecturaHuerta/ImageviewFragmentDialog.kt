340c3026ef37e11c8076913a422f98dcd2f318d9
crixtiam <cristiam9211@gmail.com>
2021-07-06 23:43:11 -0500

    keep sync

---
+++ app/src/main/java/com/darioArevalo/biblioisais/ui/main/lecturaHuerta/ImageviewFragmentDialog.kt
@@ -0,0 +1,38 @@
+package com.darioArevalo.biblioisais.ui.main.lecturaHuerta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.darioArevalo.biblioisais.R
+
+
+class ImageviewFragmentDialog : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_imageview_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView(view)
+        //setupClickListeners(view)
+    }
+
+    private fun setupView(view: View) {
+
+    }
+
+}
