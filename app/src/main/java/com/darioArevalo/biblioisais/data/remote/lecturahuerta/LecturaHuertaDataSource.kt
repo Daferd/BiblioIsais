3561165375ce387cdb3f9c50f1c53d55fd0d3015
darioArevalo <daferd19@gmail.com>
2021-07-06 23:30:31 -0500

    Actualizaciones Cristiam

--- app/src/main/java/com/darioArevalo/biblioisais/data/remote/lecturahuerta/LecturaHuertaDataSource.kt
+++ app/src/main/java/com/darioArevalo/biblioisais/data/remote/lecturahuerta/LecturaHuertaDataSource.kt
@@ -10,18 +10,15 @@ import com.darioArevalo.biblioisais.core.Result
 import com.darioArevalo.biblioisais.data.model.PostServer
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
-import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.auth.User
 import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import kotlinx.coroutines.tasks.await
 import java.io.ByteArrayOutputStream
 import java.util.*
-import kotlin.properties.Delegates
 
 class LecturaHuertaDataSource {
 
@@ -37,71 +34,96 @@ class LecturaHuertaDataSource {
     }
 
     fun setPost(autor:String, contenido:String, titulo:String, date: String, bitmap: Bitmap){
-         val querySnapshot = FirebaseFirestore.getInstance().collection("postblog")
-         val user = FirebaseAuth.getInstance().currentUser
-         uuid = UUID.randomUUID()
-         downloadTask = ""
+        val querySnapshot = FirebaseFirestore.getInstance().collection("postblog")
+        uuid = UUID.randomUUID()
+        downloadTask = ""
 
 
-         var User_Id = user?.uid.toString()
-         val user_name = user?.displayName.toString()
-         val storaRef = FirebaseStorage.getInstance().reference
-         val imageRef = storaRef.child("fotosPost/" + uuid.toString())
-         val baos = ByteArrayOutputStream()
-         bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos)
-         val data = baos.toByteArray()
-         val uploadTask = imageRef.putBytes(data)
-
-        val time_server =  Timestamp.now()
-        val time_created = time_server.seconds * 1000 + time_server.nanoseconds / 1000000
+        var post_Id = ""
+        val storaRef = FirebaseStorage.getInstance().reference
+        val imageRef = storaRef.child("fotosPost/" + uuid.toString())
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
 
 
-         uploadTask.continueWithTask{ task->
-             if (!task.isSuccessful){
-                 task.exception?.let { exception->
-                     throw exception
+        uploadTask.continueWithTask{ task->
+            if (!task.isSuccessful){
+                task.exception?.let { exception->
+                    throw exception
+                }
+            }
+            imageRef.downloadUrl
+        }.addOnCompleteListener {task->
+            if (task.isSuccessful){
+                downloadTask = task.result.toString()
+                post_Id = querySnapshot.document().id
+                val postHashMap = hashMapOf(
+                    "autor" to  autor,
+                    "contenido" to contenido,
+                    "titulo" to titulo,
+                    "timestamp" to FieldValue.serverTimestamp(),
+                    "post_image" to downloadTask,
+                    "post_Id" to post_Id
+                )
+                querySnapshot.document(post_Id)
+                    .set(postHashMap)
+                    .addOnCompleteListener{
+                        if (it.isSuccessful){
+                            //message for succesfull
+                        }else{
+                            //message for failure
+                        }
+                    }
+            }else{
+                Log.d("storage","fallo App ${task.isSuccessful}   ")
+            }
+        }
+
+
+/*
+         Log.d("storageUriOut","value ${downloadTask}")
+         post_Id = querySnapshot.document().id
+         val postHashMap = hashMapOf(
+             "autor" to  autor,
+             "contenido" to contenido,
+             "titulo" to titulo,
+             "timestamp" to FieldValue.serverTimestamp(),
+             "post_image" to downloadTask,
+             "post_Id" to post_Id
+         )
+         querySnapshot.document(post_Id)
+             .set(postHashMap)
+             .addOnCompleteListener{
+                 if (it.isSuccessful){
+                     //message for succesfull
+                 }else{
+                     //message for failure
                  }
-             }
-             imageRef.downloadUrl
-         }.addOnCompleteListener {task->
-             if (task.isSuccessful){
-                 downloadTask = task.result.toString()
-                 val post_Id = querySnapshot.document().id
-                 val postHashMap = hashMapOf(
-                     "autor" to  user_name,
-                     "contenido" to contenido,
-                     "titulo" to titulo,
-                     "timestamp" to FieldValue.serverTimestamp(),
-                     "post_image" to downloadTask,
-                     "post_Id" to post_Id,
-                     "User_Id" to User_Id,
-                     "created_at" to time_created
-                 )
-                 querySnapshot.document(post_Id)
-                     .set(postHashMap)
-                     .addOnCompleteListener{
-                         if (it.isSuccessful){
-                             //message for succesfull
-                         }else{
-                             //message for failure
-                         }
-                     }
-             }else{
-                 Log.d("storage","fallo App ${task.isSuccessful}   ")
-             }
-         }
+             }*/
+
+        //querySnapshot.document(post_Id).collection("comentarios_post").document()
+
+        /*querySnapshot.add(postHashMap).addOnCompleteListener {
+            if (it.isSuccessful){
+                    //message for succesfull
+            }else{
+                    //message for failure
+            }
+        }*/
 
 
-
-
-     }
+    }
 
     companion object {
         private lateinit var downloadTask : String
         private lateinit var uuid: UUID
     }
 
-    }
+
+
+}
 
 
 
@@ -111,5 +133,15 @@ class LecturaHuertaDataSource {
 
 
 
-
-
+/*
+val user = FirebaseAuth.getInstance().currentUser
+        var userID = ""
+        user?.let {
+            userID = user.uid
+            val db = FirebaseFirestore.getInstance()
+            db.collection("users").document(userID).get().await()
+        }
+*
+*
+*
+* */
