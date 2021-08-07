c9fe7330f1c0ef27ce5fa75445b5d9cadd31434b
crixtiam <cristiam9211@gmail.com>
2021-08-07 01:26:30 -0500

    new changes x2

--- app/src/main/java/com/darioArevalo/biblioisais/domain/lecturahuerta/CommentPostRepo.kt
+++ app/src/main/java/com/darioArevalo/biblioisais/domain/lecturahuerta/CommentPostRepo.kt
@@ -4,7 +4,6 @@ import com.darioArevalo.biblioisais.core.Result
 import com.darioArevalo.biblioisais.data.model.CommentPost
 
 interface CommentPostRepo {
-
     suspend fun suspend_get_comments(keyPost:String):Result<List<CommentPost>>
     fun getLatestComments(keyPost:String): Result<List<CommentPost>>
     fun addNewComment(content:String,keyPost: String)
