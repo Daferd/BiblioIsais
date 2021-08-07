51b924475be47c234e2da3f4a9af7ce7e5237061
crixtiam <cristiam9211@gmail.com>
2021-08-07 01:24:29 -0500

    new changes x2

--- app/src/main/java/com/darioArevalo/biblioisais/domain/lecturahuerta/CommentPostRepoImp.kt
+++ app/src/main/java/com/darioArevalo/biblioisais/domain/lecturahuerta/CommentPostRepoImp.kt
@@ -5,6 +5,7 @@ import com.darioArevalo.biblioisais.data.model.CommentPost
 import com.darioArevalo.biblioisais.data.remote.lecturahuerta.CommentPostDataSource
 
 class CommentPostRepoImp(private val dataSource: CommentPostDataSource): CommentPostRepo {
+
     override suspend fun suspend_get_comments(keyPost:String): Result<List<CommentPost>> = dataSource.suspend_get_comments(keyPost)
     override fun getLatestComments(keyPost:String): Result<List<CommentPost>> = dataSource.getLatestComments(keyPost)
     override fun addNewComment(content:String,keyPost: String) = dataSource.addNewComment(content,keyPost)
